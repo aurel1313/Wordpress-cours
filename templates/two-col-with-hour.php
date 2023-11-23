62c4cb6aee31463f9292f65b2ef043a3f4fd360e
Aurelien FABRE <aurelien.fabre2022@campus-eni.fr>
2023-11-23 09:09:28 +0100

    elementor

--- templates/two-col-with-hour.php
+++ templates/two-col-with-hour.php
@@ -7,17 +7,14 @@
 ?>
 <?php  get_header(); ?>
 <body>
-<div class="container">
-    <div class="row">
-      <div class="col-md-6">
+
+   
+     
         <?php the_content(); ?>
-      </div>
-      <div class="col-md-6 text-right">
-        <h1>Heure</h1>
-        <p id="heure"><?= date("H:i:s" ); ?></p>
-      </div>
+     
+     
     </div>
-  </div>
+ 
 </body>
 </html>
 </body>
